45966e6643a0933b80b805032420c81666523bf4
Rahul Debnath <67674099+RDNATH93@users.noreply.github.com>
2025-06-22 10:35:17 +0000

    ai service demo

--- langchain/app/src/main/java/langchain/aiservice/AiServicesWithToolsDemo.java
+++ langchain/app/src/main/java/langchain/aiservice/AiServicesWithToolsDemo.java
@@ -4,30 +4,33 @@ import dev.langchain4j.agent.tool.Tool;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 
-
-interface MathAssistant{
+interface MathAssistant {
     String solve(String userMessage);
-} 
+}
 
 class Calculator {
 
     @Tool("add two numbers")
     long add(int num1, int num2) {
+        System.out.println("doing addition...");
         return num1 + num2;
     }
 
     @Tool("substract two numbers")
     int substract(int num1, int num2) {
+        System.out.println("doing substraction...");
         return num1 + num2;
     }
 
     @Tool("multiply two numbers")
     long multiply(int num1, int num2) {
+        System.out.println("doing multiplication...");
         return num1 * num2;
     }
 
     @Tool("divide two numbers")
     float divide(int num1, int num2) {
+        System.out.println("doing division...");
         if (num2 == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
 
@@ -38,24 +41,28 @@ class Calculator {
 
 public class AiServicesWithToolsDemo {
     public static void main(String[] args) {
+
         OpenAiChatModel model = OpenAiChatModel.builder()
-                .baseUrl("https://openrouter.ai/api/v1")
-                .apiKey(System.getenv("OPEN_ROUTER_GEMMA_3_27B"))
-                .modelName("google/gemma-3-27b-it:free")
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .logRequests(true)
+                .logResponses(true)
                 .build();
 
         MathAssistant mathAssistant = AiServices.builder(MathAssistant.class)
-        .chatModel(model)
-        .tools(new Calculator()) 
-        .build();  
-        
+                .chatModel(model)
+                .systemMessageProvider((s)->"solve problem step by step")
+                .tools(new Calculator())
+                .build();
+
         String answer = mathAssistant.solve("""
-                Emma had 48 apples. She gave 12 apples to her friend, then bought 20 more apples. 
-                She divided all her apples equally into 4 baskets. 
+                Emma had 48 apples. She gave 12 apples to her friend, then bought 20 more apples.
+                She divided all her apples equally into 4 baskets.
                 Each basket of apples is then multiplied by 3 to prepare apple gift packs.
-                How many apples are in total after the final multiplication?
+                How many apples are in total ?
                 """);
-        
-       System.out.println(answer);         
+
+        System.out.println(answer);
     }
 }
